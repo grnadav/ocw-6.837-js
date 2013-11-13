4c93144e07191cece237c9267eb2234396d0799c
Nadav Greenberg <grnadav@gmail.com>
2013-11-13 11:42:00 +0200

    expose vec4 via vectors and test

--- assignment01-ifs/src/js/vectors.js
+++ assignment01-ifs/src/js/vectors.js
@@ -1,10 +1,12 @@
 define([
     'vec2',
-    'vec3'
-], function (vec2, vec3) {
+    'vec3',
+    'vec4'
+], function (vec2, vec3, vec4) {
 
     return {
         vec2: vec2,
-        vec3: vec3
+        vec3: vec3,
+        vec4: vec4
     };
 });
--- assignment01-ifs/test/js/vectors.spec.js
+++ assignment01-ifs/test/js/vectors.spec.js
@@ -2,8 +2,9 @@ define([
     'vectors',
 
     'vec2',
-    'vec3'
-], function(vectors, vec2, vec3) {
+    'vec3',
+    'vec4'
+], function(vectors, vec2, vec3, vec4) {
     describe('vectors', function() {
         it('should expose vec2 constructor', function() {
             expect(vectors.vec2).to.be(vec2);
@@ -11,6 +12,10 @@ define([
 
         it('should expose vec3 constructor', function() {
             expect(vectors.vec3).to.be(vec3);
+        });
+
+        it('should expose vec4 constructor', function() {
+            expect(vectors.vec4).to.be(vec4);
         });
     });
 });

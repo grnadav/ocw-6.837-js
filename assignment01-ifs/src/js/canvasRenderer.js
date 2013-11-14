c5ea8b87ea79e6449c9131f392b97aa4d526c4d8
Nadav Greenberg <grnadav@gmail.com>
2013-11-15 00:07:05 +0200

    canvas renderer to show results on the canvas

---
+++ assignment01-ifs/src/js/canvasRenderer.js
@@ -0,0 +1,53 @@
+define([],
+    function () {
+
+        var canvasRenderer = (function () {
+            var constr = function(canvasEl) {
+                this.canvas = canvasEl;
+                this.ctx = this.canvas.getContext('2d');
+
+                // 6300 - Rotem Barak, stills only - maybe 6000
+                // video - shooting 2 weeks before 4300
+            };
+
+            function setCanvasSize(width, height) {
+                this.canvas.setAttribute('width', width+'px');
+                this.canvas.setAttribute('height', height+'px');
+            }
+
+            function clear(width, height) {
+                this.ctx.fillStyle = "white";
+                this.ctx.fillRect(0, 0, width, height);
+            }
+
+            function setPixel(x,y,colorVec) {
+                var colorStr = 'rgb('+colorVec.r()+','+colorVec.g()+','+colorVec.b()+')';
+                this.ctx.fillStyle = colorStr;
+                this.ctx.fillRect(x, y, 1, 1);
+            }
+
+            function render(image) {
+                var x, y,
+                    width = image.width(), height = image.height();
+                setCanvasSize.call(this, width, height);
+                clear.call(this, width, height);
+                for (x=0; x<width; x++) {
+                    for (y=0; y<height; y++) {
+                        setPixel.call(this, x,y, image.getPixel(x,y));
+                    }
+                }
+            }
+
+            constr.prototype = {
+                constructor: canvasRenderer,
+
+                render: render
+            }
+
+            return constr;
+
+        })();
+
+        return canvasRenderer;
+    }
+);

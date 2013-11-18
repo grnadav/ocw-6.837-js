87afbba5e33e70e85a2442a112ec516466010b4c
Nadav Greenberg <grnadav@gmail.com>
2013-11-18 23:42:51 +0200

    orthographic camera tests

--- assignment01-ray-casting/test/js/orthographicCamera.spec.js
+++ assignment01-ray-casting/test/js/orthographicCamera.spec.js
@@ -12,5 +12,45 @@ define([
             expect(s).to.be.a(orthographicCamera);
         });
 
+        it('should generate rays', function() {
+            var center = new vec3(0,0,0),
+                up = new vec3(0,1,0),
+                direction = new vec3(0,0,1),
+                size = 3,
+                orthCam = new orthographicCamera(center, direction, up, size),
+                point = new vec2(0,0),
+                ray;
+
+            ray = orthCam.generateRay(point);
+
+            expect(ray.getOrigin().x()).to.be(1.5);
+            expect(ray.getOrigin().y()).to.be(-1.5);
+            expect(ray.getOrigin().z()).to.be(0);
+
+            expect(ray.getDirection().x()).to.be(0);
+            expect(ray.getDirection().y()).to.be(0);
+            expect(ray.getDirection().z()).to.be(1);
+        });
+
+        it('should generate rays and normalize input', function() {
+            var center = new vec3(0,0,0),
+                up = new vec3(0,4,0),
+                direction = new vec3(0,0,6),
+                size = 3,
+                orthCam = new orthographicCamera(center, direction, up, size),
+                point = new vec2(0,0),
+                ray;
+
+            ray = orthCam.generateRay(point);
+
+            expect(ray.getOrigin().x()).to.be(1.5);
+            expect(ray.getOrigin().y()).to.be(-1.5);
+            expect(ray.getOrigin().z()).to.be(0);
+
+            expect(ray.getDirection().x()).to.be(0);
+            expect(ray.getDirection().y()).to.be(0);
+            expect(ray.getDirection().z()).to.be(1);
+        });
+
     });
 });
